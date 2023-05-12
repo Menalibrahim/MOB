047e2fe6175dae46d2a2f68f629c8c8cd724b8f4
amminadab <amminadabeliasfant@gmail.com>
2023-05-12 14:13:31 +0300

    fixed the routing issue

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {HomePage, MyFooter} from "./pages";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomePage, MyFooter, NavBar } from "./pages";
 import { IphoneProduct, SamsungProduct } from "./components/products";
 import TechnoProduct from "./components/products/TechnoP";
 
-
 function App() {
-  return(
-  <BrowserRouter>
-      <Routes>
-       {/* <Route path="/" element={<NavBar/>}/> */} 
-        <Route index element={<HomePage />}/>
-        <Route path="iphone" element={<IphoneProduct/>}/>
-        <Route path="samsung" element={<SamsungProduct/>}/>
-        <Route path="techno" element={<TechnoProduct/>}/>
-      </Routes>
-          </BrowserRouter>
-       );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <NavBar />,
+      children: [
+        { index: "index", element: <HomePage /> },
+        { path: "product", element: <HomePage /> },
+        { path: "checkout", element: <IphoneProduct /> },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
