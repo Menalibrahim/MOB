556d427744eac074fee5564b83bd0c6c8101e455
amminadab <amminadabeliasfant@gmail.com>
2023-05-13 00:17:40 +0300

    made the footer responsive

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,21 +1,35 @@
+import { Link } from "react-router-dom";
 
-
-function MyFooter(){
-    return(
-        <section id="bootom-navbar" className="fixed-bottom d-xs-block d-sm-block d-md-block d-lg-none" style={{background: "rgb(58, 181, 74)",marginTop: "200px"}}>
-            <div className="d-flex justify-content-between fixed-bottom px-3" style={{background: "#2cb1bc"}}>
-            <div className="text-center mt-2"><a href="Store.html"> <i style={{color:"white"}} className="fa fa-shopping-cart mr-1" /></a>
-                <p style={{fontSize: "14px"}}><strong>Store</strong></p>
-            </div>
-            <div className="text-center mt-2"><a href="Login"><i style={{color:"white"}}  class="fa fa-sign-in" ></i></a>
-                <p style={{fontSize: "14px"}}><strong>Login</strong></p>
-            </div> 
-            <div className="text-center mt-2"><a href="Product.html"><i style={{color:"white"}} class="fa fa-user" ></i></a>
-                <p style={{fontSize: "14px"}}><strong>Profile</strong></p>
-            </div> 
-            </div>
-        </section>
-    )
+function MyFooter() {
+  return (
+    <section
+      id="bootom-navbar"
+      className="fixed-bottom d-xs-block d-sm-block d-md-block d-lg-none"
+      style={{ background: "rgb(58, 181, 74)", marginTop: "200px" }}
+    >
+      <div
+        className="d-flex justify-content-between fixed-bottom px-3"
+        style={{ background: "#2cb1bc" }}
+      >
+        <Link className="footer-link" to="/">
+          <div className="text-center mt-2">
+            <i style={{ color: "white" }} className="fa fa-home mr-1" />
+            <p style={{ fontSize: "14px" }}>
+              <strong>Home</strong>
+            </p>
+          </div>
+        </Link>
+        <Link className="footer-link" to="products">
+          <div className="text-center mt-2">
+            <i style={{ color: "white" }} class="fa fa-list"></i>
+            <p style={{ fontSize: "14px" }}>
+              <strong>Product</strong>
+            </p>
+          </div>
+        </Link>
+      </div>
+    </section>
+  );
 }
 
 export default MyFooter;
