a9df6cfdceb62b76b466a973dab8a1483b6cca91
amminadab <amminadabeliasfant@gmail.com>
2023-05-12 21:31:01 +0300

    modified the page to have a button

--- src/assets/wrappers/DescriptionPage.js
+++ src/assets/wrappers/DescriptionPage.js
@@ -1,55 +1,63 @@
-import styled from 'styled-components'
-
+import styled from "styled-components";
 
 const Wrapper = styled.main`
-.page {
-  min-height: calc(100vh - var(--nav-height));
-  display: grid;
-  align-items: center;
-  margin-top: -3rem;
-  
-}
-nav {
-  width: var(--fluid-width);
-  max-width: var(--max-width);
-  margin: 0 auto;
-  height: var(--nav-height);
-  display: flex;
-  align-items: center;
-}
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
 
-h1{
-  font-weight: 700;
-  margin-top: 10rem;
-  margin-left:10rem;
-  span{
-    color: var(--primary-600);
-  }
-}
-p{
-  color: var(--grey-600);
-  margin-left:10rem
-}
-.img{
-  
-  margin-top:10rem;
-  width: 25%;
-  transform: rotate(25deg);
-  position: absolute;
-  right: 20%;
+  h1 {
+    font-weight: 700;
+    margin-top: 10rem;
 
-  height: 450px;
-  border-radius: 25%;
-}
-@media(min-width: 900px){
-  .page{
-    grid-template-columns: 1fr 1fr;
-    column-gap: 3rem;
+    span {
+      color: var(--primary-600);
+    }
   }
-  .main-img{
-    display: block;
-    
+  p {
+    color: var(--grey-600);
   }
-}
-`
-export default Wrapper
+  .img {
+    margin-top: 10rem;
+    width: 25%;
+    transform: rotate(25deg);
+    position: absolute;
+    right: 20%;
+
+    height: 450px;
+    border-radius: 25%;
+  }
+  button {
+    padding: 0.6rem 1.4rem;
+    background-color: var(--primary-600);
+    border: none;
+    color: white;
+    font-weight: 600;
+    border-radius: 10px;
+    transition: all 0.5s;
+  }
+  button:hover {
+    padding: 0.6rem 1.4rem;
+    background-color: var(--primary-700);
+  }
+  @media (min-width: 900px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`;
+export default Wrapper;
--- src/components/DescriptionPage/DescriptionPage.js
+++ src/components/DescriptionPage/DescriptionPage.js
@@ -1,28 +1,38 @@
-import Wrapper from "../../assets/wrappers/DescriptionPage"
+import Wrapper from "../../assets/wrappers/DescriptionPage";
 
 function DescriptionPage() {
   return (
     <Wrapper>
-    <div className="container-fluid">
-    <div className="row">
-      <div className="col-lg-6">
-           <h1 class="heading">Amazing Phone.<br/><span>Amazing Deals.</span></h1>
-           <p>simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <h1 class="heading">
+              Amazing Phone.
+              <br />
+              <span>Amazing Deals.</span>
+            </h1>
+            <p className="margin-tt">
+              Experience the ultimate mobile upgrade with our online store.
+              Choose from a wide range of brands and models, all backed by our
+              100% satisfaction guarantee. Enjoy lightning-fast delivery, 1-year
+              warranty, and exceptional customer service. Elevate your phone
+              game today and shop with us!
+            </p>
+            <button className="margin-tt">Show Products</button>
+          </div>
+
+          <div class="col-lg-6">
+            <img
+              style={{ height: "300px", width: "300px" }}
+              className="img"
+              src={require("../../assets/images/mob.jpg")}
+              alt="mobile phone"
+            />
+          </div>
         </div>
-
-       <div class="col-lg-6">
-       <img  style={{"height":"300px", "width":"300px"}}
-        className="img"
-        src={require("../../assets/images/mob.jpg")}
-        alt="mobile phone"
-        />
-       </div>
-
-     </div>
-     </div>
-     </Wrapper>
-    
-  )
+      </div>
+    </Wrapper>
+  );
 }
 
-export default DescriptionPage
+export default DescriptionPage;
