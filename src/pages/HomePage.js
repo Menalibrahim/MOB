34eab38ed81973bce1e3efbe071ea4114dc93008
amminadab <amminadabeliasfant@gmail.com>
2023-05-12 20:55:05 +0300

    optimied the code in the Home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,50 +1,25 @@
-import React from 'react'
+import React from "react";
 import { Fragment } from "react";
-import { DescriptionPage, IphoneItems, ContactUs, SamsungItems } from '../components/items';
-import NavBar from './NavBar';
-import About from './About';
-import TechnoItem from '../components/items/TechnoItems';
-import { TechnoProvider } from '../components/items/TechnoContext';
+import {
+  DescriptionPage,
+  IphoneItems,
+  ContactUs,
+  SamsungItems,
+} from "../components/items";
+import About from "./About";
+import Brands from "../components/brands/brands";
+import Offer from "../components/offer/offer";
 
 function HomePage() {
   return (
-   <TechnoProvider>
     <Fragment>
-      <NavBar/>
-    <DescriptionPage/>
-    <section id="company_posts" className="mt-5">
-        <div className="container-fluid px-3">
-            <div className="row">
-                <div className="col-sm-12">
-                    <h2><strong>iPhones</strong></h2>
-                    <hr/>
-                    <div className="row mt-4">
-                        <IphoneItems/>
-                        </div>
-                    </div>
-                    <div className="col-sm-12">
-                    <h2><strong>Samsungs</strong></h2>
-                    <hr/>
-                    <div className="row mt-4">
-                        <SamsungItems/>
-                        </div>
-                    <div className="col-sm-12">
-                    <h2><strong>Technos</strong></h2>
-                    <hr/>
-                    <div className="row mt-4">
-                         <TechnoItem/> 
-                        </div> 
-                        </div>  
-                    </div>
-                    </div>
-                    </div>
-                    </section>
- 
-     <ContactUs/>
-     <About/>
+      <DescriptionPage />
+      <Brands />
+      <Offer />
+      <ContactUs />
+      <About />
     </Fragment>
-    </TechnoProvider>
-  
-)}
+  );
+}
 
-export default HomePage
+export default HomePage;
