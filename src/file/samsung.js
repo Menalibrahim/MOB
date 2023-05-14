b8429e9a6b9bfe78c48b6cf483897fb2eaec7b5b
amminadab <amminadabeliasfant@gmail.com>
2023-05-14 21:13:28 +0300

    added files for the app phones

---
+++ src/file/samsung.js
@@ -0,0 +1,80 @@
+const samsung = [
+  {
+    brand_id: 9,
+    brand_name: "Samsung",
+    description:
+      "Samsung Galaxy A24 4G Android smartphone. Announced Apr 2023. Features 6.5″  display, MT8781 Helio G99 chipset, 5000 mAh battery, 128 GB storage, 8 GB RAM.",
+    id: "samsung_galaxy_a24_4g-12176",
+    image_url:
+      "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a24-4g-2.jpg",
+    phone_name: "Galaxy A24 4G",
+  },
+  {
+    brand_id: 9,
+    brand_name: "Samsung",
+    description:
+      "Samsung Galaxy F14 Android smartphone. Announced Mar 2023. Features 6.6″  display, Exynos 1330 chipset, 6000 mAh battery, 128 GB storage, 6 GB RAM, Corning Gorilla Glass 5.",
+    id: "samsung_galaxy_f14-12175",
+    image_url: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-f14.jpg",
+    phone_name: "Galaxy F14",
+  },
+  {
+    brand_id: 9,
+    brand_name: "Samsung",
+    description:
+      "Samsung Galaxy M54 Android smartphone. Announced Mar 2023. Features 6.7″  display, Exynos 1380 chipset, 6000 mAh battery, 256 GB storage, 8 GB RAM, Corning Gorilla Glass 5.",
+    id: "samsung_galaxy_m54-12189",
+    image_url: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-m54-5g.jpg",
+    phone_name: "Galaxy M54",
+  },
+  {
+    brand_id: 9,
+    brand_name: "Samsung",
+    description:
+      "Samsung Galaxy A54 Android smartphone. Announced Mar 2023. Features 6.4″  display, Exynos 1380 chipset, 5000 mAh battery, 256 GB storage, 8 GB RAM, Corning Gorilla Glass 5.",
+    id: "samsung_galaxy_a54-12070",
+    image_url: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a54.jpg",
+    phone_name: "Galaxy A54",
+  },
+  {
+    brand_id: 9,
+    brand_name: "Samsung",
+    description:
+      "Samsung Galaxy A34 Android smartphone. Announced Mar 2023. Features 6.6″  display, MT6877V Dimensity 1080 chipset, 5000 mAh battery, 256 GB storage, 8 GB RAM, Corning Gorilla Glass 5.",
+    id: "samsung_galaxy_a34-12074",
+    image_url: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a34.jpg",
+    phone_name: "Galaxy A34",
+  },
+  {
+    brand_id: 9,
+    brand_name: "Samsung",
+    description:
+      "Samsung Galaxy M14 Android smartphone. Announced Mar 2023. Features 6.6″  display, Exynos 1330 chipset, 6000 mAh battery, 128 GB storage, 6 GB RAM, Corning Gorilla Glass 5.",
+    id: "samsung_galaxy_m14-12160",
+    image_url:
+      "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-m14-5g-sm-m146.jpg",
+    phone_name: "Galaxy M14",
+  },
+  {
+    brand_id: 9,
+    brand_name: "Samsung",
+    description:
+      "Samsung Galaxy S23 Ultra Android smartphone. Announced Feb 2023. Features 6.8″  display, Snapdragon 8 Gen 2 chipset, 5000 mAh battery, 1024 GB storage, 12 GB RAM, Corning Gorilla Glass Victus 2.",
+    id: "samsung_galaxy_s23_ultra-12024",
+    image_url:
+      "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-s23-ultra-5g.jpg",
+    phone_name: "Galaxy S23 Ultra",
+  },
+  {
+    brand_id: 9,
+    brand_name: "Samsung",
+    description:
+      "Samsung Galaxy S23+ Android smartphone. Announced Feb 2023. Features 6.6″  display, Snapdragon 8 Gen 2 chipset, 4700 mAh battery, 512 GB storage, 8 GB RAM, Corning Gorilla Glass Victus 2.",
+    id: "samsung_galaxy_s23+-12083",
+    image_url:
+      "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-s23-plus-5g.jpg",
+    phone_name: "Galaxy S23+",
+  },
+];
+
+export default samsung;
