3df3eb1118263631ebcbcc3abab22b99d3c525dc
amminadab <amminadabeliasfant@gmail.com>
2023-05-14 21:35:42 +0300

    added tecno products

---
+++ src/file/tecno.js
@@ -0,0 +1,77 @@
+const Tecno = [
+  {
+    brand_id: 120,
+    brand_name: "Tecno",
+    description:
+      "Tecno Camon 20 Premier Android smartphone. Announced May 2023. Features 6.67″  display, Dimensity 8050 chipset, 5000 mAh battery, 512 GB storage, 8 GB RAM.",
+    id: "tecno_camon_20_premier-12256",
+    image_url:
+      "https://fdn2.gsmarena.com/vv/bigpic/tecno-camon20-premier-5g.jpg",
+    phone_name: "Camon 20 Premier",
+  },
+  {
+    brand_id: 120,
+    brand_name: "Tecno",
+    description:
+      "Tecno Camon 20 Pro 5G Android smartphone. Announced May 2023. Features 6.67″  display, Dimensity 8050 chipset, 5000 mAh battery, 256 GB storage, 8 GB RAM.",
+    id: "tecno_camon_20_pro_5g-12255",
+    image_url: "https://fdn2.gsmarena.com/vv/bigpic/tecno-camon20-pro-5g.jpg",
+    phone_name: "Camon 20 Pro 5G",
+  },
+  {
+    brand_id: 120,
+    brand_name: "Tecno",
+    description:
+      "Tecno Camon 20 Pro Android smartphone. Announced May 2023. Features 6.67″  display, MT8781 Helio G99 chipset, 5000 mAh battery, 256 GB storage, 8 GB RAM.",
+    id: "tecno_camon_20_pro-12254",
+    image_url: "https://fdn2.gsmarena.com/vv/bigpic/tecno-camon20-pro.jpg",
+    phone_name: "Camon 20 Pro",
+  },
+  {
+    brand_id: 120,
+    brand_name: "Tecno",
+    description:
+      "Tecno Camon 20 Android smartphone. Announced May 2023. Features 6.67″  display, MT6769Z Helio G85 chipset, 5000 mAh battery, 256 GB storage, 8 GB RAM.",
+    id: "tecno_camon_20-12253",
+    image_url: "https://fdn2.gsmarena.com/vv/bigpic/tecno-camon20-pro.jpg",
+    phone_name: "Camon 20",
+  },
+  {
+    brand_id: 120,
+    brand_name: "Tecno",
+    description:
+      "Tecno Pop 7 Android smartphone. Announced Mar 2023. Features 6.6″  display, 5000 mAh battery, 64 GB storage, 2 GB RAM.",
+    id: "tecno_pop_7-12202",
+    image_url: "https://fdn2.gsmarena.com/vv/bigpic/tecno-pop7.jpg",
+    phone_name: "Pop 7",
+  },
+  {
+    brand_id: 120,
+    brand_name: "Tecno",
+    description:
+      "Tecno Spark 10 5G Android smartphone. Announced Mar 2023. Features 6.6″  display, Dimensity 6020 chipset, 5000 mAh battery, 256 GB storage, 8 GB RAM.",
+    id: "tecno_spark_10_5g-12179",
+    image_url: "https://fdn2.gsmarena.com/vv/bigpic/tecno-spark10-5g.jpg",
+    phone_name: "Spark 10 5G",
+  },
+  {
+    brand_id: 120,
+    brand_name: "Tecno",
+    description:
+      "Tecno Spark 10C Android smartphone. Announced Mar 2023. Features 6.6″  display, 5000 mAh battery, 128 GB storage, 8 GB RAM.",
+    id: "tecno_spark_10c-12199",
+    image_url: "https://fdn2.gsmarena.com/vv/bigpic/tecno-spark-10c.jpg",
+    phone_name: "Spark 10C",
+  },
+  {
+    brand_id: 120,
+    brand_name: "Tecno",
+    description:
+      "Tecno Spark 10 Android smartphone. Announced Mar 2023. Features 6.6″  display, MT6765 Helio G37 chipset, 5000 mAh battery, 128 GB storage, 8 GB RAM.",
+    id: "tecno_spark_10-12178",
+    image_url: "https://fdn2.gsmarena.com/vv/bigpic/tecno-spark10.jpg",
+    phone_name: "Spark 10",
+  },
+];
+
+export default Tecno;
