34b509d7db6681dc8dd2005ca57418f5ff643c20
amminadab <amminadabeliasfant@gmail.com>
2023-05-14 21:35:21 +0300

    added iphone products

---
+++ src/file/iphone.js
@@ -0,0 +1,78 @@
+const Iphone = [
+  {
+    brand_id: 48,
+    brand_name: "Apple",
+    description:
+      "Apple iPad Pro 12.9 (2022) tablet. Announced Oct 2022. Features 12.9″  display, Apple M2 chipset, 10758 mAh battery, 2048 GB storage, 16 GB RAM, Scratch-resistant glass.",
+    id: "apple_ipad_pro_12.9_(2022)-11939",
+    image_url:
+      "https://fdn2.gsmarena.com/vv/bigpic/apple-ipad-pro-129-2022.jpg",
+    phone_name: "iPad Pro 12.9 (2022)",
+  },
+  {
+    brand_id: 48,
+    brand_name: "Apple",
+    description:
+      "Apple iPad Pro 11 (2022) tablet. Announced Oct 2022. Features 11.0″  display, Apple M2 chipset, 7538 mAh battery, 2048 GB storage, 16 GB RAM, Scratch-resistant glass.",
+    id: "apple_ipad_pro_11_(2022)-11940",
+    image_url: "https://fdn2.gsmarena.com/vv/bigpic/apple-ipad-pro-11-2022.jpg",
+    phone_name: "iPad Pro 11 (2022)",
+  },
+  {
+    brand_id: 48,
+    brand_name: "Apple",
+    description:
+      "Apple iPad (2022) tablet. Announced Oct 2022. Features 10.9″  display, Apple A14 Bionic chipset, 7606 mAh battery, 256 GB storage, 4 GB RAM, Scratch-resistant glass.",
+    id: "apple_ipad_(2022)-11941",
+    image_url: "https://fdn2.gsmarena.com/vv/bigpic/apple-ipad-10-2022.jpg",
+    phone_name: "iPad (2022)",
+  },
+  {
+    brand_id: 48,
+    brand_name: "Apple",
+    description:
+      "Apple iPhone 14 Pro Max smartphone. Announced Sep 2022. Features 6.7″  display, Apple A16 Bionic chipset, 4323 mAh battery, 1024 GB storage, 6 GB RAM, Ceramic Shield glass.",
+    id: "apple_iphone_14_pro_max-11773",
+    image_url:
+      "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-14-pro-max-.jpg",
+    phone_name: "iPhone 14 Pro Max",
+  },
+  {
+    brand_id: 48,
+    brand_name: "Apple",
+    description:
+      "Apple iPhone 14 Pro smartphone. Announced Sep 2022. Features 6.1″  display, Apple A16 Bionic chipset, 3200 mAh battery, 1024 GB storage, 6 GB RAM, Ceramic Shield glass.",
+    id: "apple_iphone_14_pro-11860",
+    image_url: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-14-pro.jpg",
+    phone_name: "iPhone 14 Pro",
+  },
+  {
+    brand_id: 48,
+    brand_name: "Apple",
+    description:
+      "Apple iPhone 14 Plus smartphone. Announced Sep 2022. Features 6.7″  display, Apple A15 Bionic chipset, 4323 mAh battery, 512 GB storage, 6 GB RAM, Ceramic Shield glass.",
+    id: "apple_iphone_14_plus-11862",
+    image_url: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-14-plus.jpg",
+    phone_name: "iPhone 14 Plus",
+  },
+  {
+    brand_id: 48,
+    brand_name: "Apple",
+    description:
+      "Apple iPhone 14 smartphone. Announced Sep 2022. Features 6.1″  display, Apple A15 Bionic chipset, 3279 mAh battery, 512 GB storage, 6 GB RAM, Ceramic Shield glass.",
+    id: "apple_iphone_14-11861",
+    image_url: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-14.jpg",
+    phone_name: "iPhone 14",
+  },
+  {
+    brand_id: 48,
+    brand_name: "Apple",
+    description:
+      "Apple Watch Ultra watch. Announced Sep 2022. Features 1.92″  display, Apple S8 chipset, 542 mAh battery, 32 GB storage, MIL-STD 810H certified, Sapphire crystal glass.",
+    id: "apple_watch_ultra-11827",
+    image_url: "https://fdn2.gsmarena.com/vv/bigpic/apple-watch-ultra.jpg",
+    phone_name: "Watch Ultra",
+  },
+];
+
+export default Iphone;
