e0e97d0447ed5f493ac38708085cefc615fdb886
amminadab <amminadabeliasfant@gmail.com>
2023-05-16 01:39:41 +0300

    changed the about page

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import "normalize.css";
 import "./index.css";
 import App from "./App";
 import { PhoneDataProvider } from "./context/phoneData.context";
+import { CartProvider } from "react-use-cart";
+import { CartHolderProvider } from "./context/cartHolder.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <PhoneDataProvider>
-      <App />
-    </PhoneDataProvider>
+    <CartHolderProvider>
+      <CartProvider>
+        <PhoneDataProvider>
+          <App />
+        </PhoneDataProvider>
+      </CartProvider>
+    </CartHolderProvider>
   </React.StrictMode>
 );
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,70 +1,69 @@
-
-
 function About() {
-    return(
-        <div className="container">
-        <div className="row">
-            <div className="col-sm-12 col-md-6 col-lg-3 mb-3">
-            <h6>Posts</h6>
-            <hr />
-            <div className="mb-2">
-                <a className="text-secondary" href="browse.html">
-                Browse
-                </a>
-            </div>
-            <div className="mb-2">
-                <a className="text-secondary" href="post.html">
-                Post
-                </a>
-            </div>
-            <div className="mb-2">
-                <a className="text-secondary" href="cart.html">
-                Cart
-                </a>
-            </div>
-            </div>
-            <div className="col-sm-12 col-md-6 col-lg-3 mb-3">
-            <h6>Pages</h6>
-            <hr />
-            <div className="mb-2">
-                <a className="text-secondary" href="browse.html">
-                About
-                </a>
-            </div>
-            <div className="mb-2">
-                <a className="text-secondary" href="post.html">
-                Guideline
-                </a>
-            </div>
-            <div className="mb-2">
-                <a className="text-secondary" href="cart.html">
-                Report
-                </a>
-            </div>
-            <div className="mb-2">
-                <a className="text-secondary" href="cart.html">
-                Contact us
-                </a>
-            </div>
-            <div className="mb-2">
-                <a className="text-secondary" href="termspolicy.html">
-                Terms &amp; Policy
-                </a>
-            </div>
-            </div>
-            <div className="col-sm-12 col-lg-6 mb-3">
-            <h6>About</h6>
-            <hr />
-            <p className="text-secondary">
-            We operate 11 MOB retail outlets across Addis Ababa offering a wide variety of Mobile products at affordable prices.
-            </p>
-            </div>
-            <div className="col-sm-12 text-center text-secondary mt-5">
-            <p>Copyright @ MOB 2023</p>
-            </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-12 col-md-6 col-lg-3 mb-3">
+          <h6>Posts</h6>
+          <hr />
+          <div className="mb-2">
+            <a className="text-secondary" href="browse.html">
+              Browse
+            </a>
+          </div>
+          <div className="mb-2">
+            <a className="text-secondary" href="post.html">
+              Post
+            </a>
+          </div>
+          <div className="mb-2">
+            <a className="text-secondary" href="cart.html">
+              Cart
+            </a>
+          </div>
         </div>
+        <div className="col-sm-12 col-md-6 col-lg-3 mb-3">
+          <h6>Pages</h6>
+          <hr />
+          <div className="mb-2">
+            <a className="text-secondary" href="browse.html">
+              About
+            </a>
+          </div>
+          <div className="mb-2">
+            <a className="text-secondary" href="post.html">
+              Guideline
+            </a>
+          </div>
+          <div className="mb-2">
+            <a className="text-secondary" href="cart.html">
+              Report
+            </a>
+          </div>
+          <div className="mb-2">
+            <a className="text-secondary" href="cart.html">
+              Contact us
+            </a>
+          </div>
+          <div className="mb-2">
+            <a className="text-secondary" href="termspolicy.html">
+              Terms &amp; Policy
+            </a>
+          </div>
         </div>
-    )
+        <div className="col-sm-12 col-lg-6 mb-3">
+          <h6>About</h6>
+          <hr />
+          <p className="text-secondary">
+            We operate 11 MOB retail outlets across Addis Ababa offering a wide
+            variety of Mobile products at affordable prices.
+          </p>
+        </div>
+        <div className="col-sm-12 text-center text-secondary mt-5">
+          <p>Copyright @ MOB 2023</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
