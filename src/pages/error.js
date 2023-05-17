b85ca7d7ae7ada6d8f858bf6752a17ea322c9457
amminadab <amminadabeliasfant@gmail.com>
2023-05-17 14:02:39 +0300

    added an error page that says page not found

---
+++ src/pages/error.js
@@ -0,0 +1,14 @@
+import NavBar from "./NavBar";
+
+const Error = () => {
+  return (
+    <>
+      <NavBar />
+      <div className="page-404">
+        <p className="error-404">404 PAGE NOT FOUND</p>
+      </div>
+    </>
+  );
+};
+
+export default Error;
