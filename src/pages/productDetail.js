6b6f5475a60f63498ca1dffc75d33f501266f149
amminadab <amminadabeliasfant@gmail.com>
2023-05-17 17:46:25 +0300

    made the products detail page to show the detailed product and similar products

--- src/pages/productDetail.js
+++ src/pages/productDetail.js
@@ -1,6 +1,9 @@
 import { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { PhoneDataContext } from "../context/phoneData.context";
+import Error from "./error";
+import Detail from "../components/detail/detail";
+import Featured from "../components/featured/featured";
 
 const ProductDetail = () => {
   const { productId } = useParams();
@@ -9,10 +12,17 @@ const ProductDetail = () => {
 
   const selectedProduct = allPhones.filter((phone) => phone.id === productId);
 
-  console.log(selectedProduct);
-  //check if the product is avalable
+  if (selectedProduct.length === 0) {
+    return <Error />;
+  }
 
-  return <div>detail{productId}</div>;
+  return (
+    <div className="container">
+      <Detail product={selectedProduct[0]} />
+      <Featured product={selectedProduct[0]} />
+    </div>
+  );
+  //check if the product is avalable
 };
 
 export default ProductDetail;
